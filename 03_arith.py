dcd6ff856253ec4c8efd4d60f76e42aae34b64e2
AdrianGerman <adrian.german1019@gmail.com>
2024-01-31 07:47:19 -0600

    arithmetic operators

---
+++ 03_arith.py
@@ -0,0 +1,10 @@
+print(10 + 2) # suma
+print(10 - 2) # resta
+print(10 * 2) # multiplicación
+print(10 / 2) # division
+# nos devuelve el residuo de la division
+print(10 % 3) # modulo
+# es una division pero solo nos retorno los enteros, si hay decimales
+# los elimina el momento de regresarnos el valor
+print(10 // 3) #  división entera 
+print( 2 ** 3)# exponenciacion, elevacion
