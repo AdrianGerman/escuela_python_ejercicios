459da94c334dc86e86d3961779696b35ecb63910
AdrianGerman <adrian.german1019@gmail.com>
2024-01-31 12:07:44 -0600

    comparison operators

---
+++ 04_comparators.py
@@ -0,0 +1,53 @@
+# >
+print("Comparador mayor que")
+print(7 > 3) #true
+print(3 > 7) #false
+print(7 > 7) #false
+print("") #salto de linea
+
+# <
+print("Comparador menor que")
+print(5 < 6) #true
+print(6 < 5) #false
+print(5 < 5) #false
+print("") #salto de linea
+
+# >=
+print("Comparador mayor o igual")
+print(2 >= 1) #true
+print(2 >= 3) #false
+print(2 >= 2) #true
+print("")
+
+# <=
+print("Comparador mayor o igual")
+print(1 <= 2) #true
+print(2 <= 1) #false
+print(2 <= 2) #true
+print("")
+
+# ==
+print("Comparador igual que")
+print(6 == 6) #true
+print(5 == 2) #false
+print("")
+
+# !=
+print("Comparador es distinto de")
+print(6 != 10) #true
+print(6 != 6) #false
+print("")
+
+# mas usos de los comparadores
+print("Mas usos de los comparadores")
+print("Apple" == "Apple") #true
+print("Apple" == "apple") #false
+
+print("1" == 1) #false
+
+age = 22
+print(age >= 18)
+
+
+
+
