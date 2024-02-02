9266d7f779ca63f80b31c9e109135a509eb0e963
AdrianGerman <adrian.german1019@gmail.com>
2024-02-02 10:30:02 -0600

    list

---
+++ 11_lists.py
@@ -0,0 +1,28 @@
+numbers = [1, 2, 3, 4]
+print(numbers)
+print(type(numbers))
+
+print('')
+task = ['make a dishes', 'play videogames']
+print(task)
+
+print('')
+types = [1, True, 'mundo']
+print(types)
+
+print('')
+print(numbers[0])
+print(task[0])
+
+print('')
+task[0] = "watch courses"
+print(task)
+
+print('')
+task[0] = 'do the dishes'
+print(task)
+
+print('')
+print(numbers[:3])
+print('¿En "types" existe un "true"? =>',True in types)
+print('¿En "types" existe el texto de "mundo? =>"',"mundo" in types)
