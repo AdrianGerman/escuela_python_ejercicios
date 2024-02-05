ce18e12bad8e7a3651e9ac349528dcbf897939d3
AdrianGerman <adrian.german1019@gmail.com>
2024-02-05 08:28:07 -0600

    while

---
+++ 17_while.py
@@ -0,0 +1,18 @@
+counter = 0
+
+# while counter < 10:
+#     counter += 1
+#     print(counter)
+    
+    
+# while counter < 20:
+#     counter += 1
+#     if counter == 15:
+#         break
+#     print(counter)
+    
+while counter < 20:
+    counter += 1
+    if counter < 15:
+        continue
+    print(counter)
