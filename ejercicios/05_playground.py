22c9b73b6dbb3284bb3534cc31b45b03d2782fb5
AdrianGerman <adrian.german1019@gmail.com>
2024-02-05 08:51:52 -0600

    playgrounds: add only positive numbers from a list

---
+++ ejercicios/05_playground.py
@@ -0,0 +1,13 @@
+my_list = [1, -1, 2, -2, 3, -3, 4, -4]
+new_list = []
+
+# Escribe tu solución 👇
+
+# agregar a la new_list los numeros positivos
+for number in my_list:
+  if number > 0:
+    new_list.append(number)
+
+
+
+print(new_list)
