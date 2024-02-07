a6295c69146f63e647e3d96393260e0e483e57b5
AdrianGerman <adrian.german1019@gmail.com>
2024-02-07 10:16:13 -0600

    dictionary comprehension

---
+++ ruta_curso2/05_dict.py
@@ -0,0 +1,30 @@
+# se multiplica la llave por 2
+dict = {}
+for i in range(1, 5):
+    dict[i] = i * 2
+print(dict)
+
+dict_v2 = {i: i * 2 for i in range(1,5)}
+print(dict_v2)
+
+import random
+
+countries = ['col', 'mex', 'bol', 'pe']
+
+population = {}
+for country in countries:
+    population[country] = random.randint(1,100)
+print(population)
+
+population_v2 = { country: random.randint(1, 100) for country in countries }
+print(population_v2)
+
+names = ['adri', 'tere', 'liz']
+ages = [27, 32, 19 ]
+
+# lista con tuplas
+print(list(zip(names, ages)))
+
+# diccionario
+new_dict = { name: age for (name, age) in zip(names,ages) }
+print(new_dict)
