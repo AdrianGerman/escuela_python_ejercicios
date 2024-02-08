95d194903f21e13ff5ba6b9e4018c2647105d22f
AdrianGerman <adrian.german1019@gmail.com>
2024-02-08 08:35:32 -0600

    functions

---
+++ ruta_curso2/07_func.py
@@ -0,0 +1,16 @@
+# Funcion para multiplicar por 2 todo el resultado obtenido
+def my_print(text):
+    print(text * 2)
+
+# Funcion que hace la suma de 2 numeros proporcionados
+def suma(a, b):
+    print(a + b)
+    
+# usando la primer función
+my_print('Este es mi texto')
+my_print('Hola, mundo')
+
+# utilizando la segunda función
+suma(10, 5)
+suma(3143, 24)
+    
