8b40ffffa98448cee8b194dd02135d56c344914f
AdrianGerman <adrian.german1019@gmail.com>
2024-02-11 10:47:40 -0600

    __name__ and __main__

--- ruta_curso2/app/main.py
+++ ruta_curso2/app/main.py
@@ -1,20 +1,26 @@
 import utils
 
-keys, values = utils.get_population()
-print(keys, values)
+def run():
+    keys, values = utils.get_population()
+    print(keys, values)
 
-data = [
-    {
-        'Country': 'Mexico',
-        'Population': 500
-    },
-    {
-        'Country': 'Paraguay',
-        'Population': 300
-    }
-]
+    data = [
+        {
+            'Country': 'Mexico',
+            'Population': 500
+        },
+        {
+            'Country': 'Paraguay',
+            'Population': 300
+        }
+    ]
 
-country = input('Escribe el país => ')
+    country = input('Escribe el país => ')
 
-result = utils.population_by_country(data, country)
-print(result)
+    result = utils.population_by_country(data, country)
+    print(result)
+    
+    
+    
+if __name__ == '__main__':
+    run()
